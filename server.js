e56d07d0dc312e85432771b058132735489022ef
bodake <sanketh.121@gmail.com>
2026-05-18 17:23:27 +0200

    Replace CORS middleware with custom headers

--- server.js
+++ server.js
@@ -5,9 +5,17 @@ const axios = require("axios");
 
 const app = express();
 
-app.use(cors({
-  origin: "*"
-}));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
