9a31f4d9f64347d4fde189cd91045d3cd50bf7bf
bodake <sanketh.121@gmail.com>
2026-05-18 17:28:34 +0200

    Update CORS headers and response status code

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const axios = require("axios");
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
+    return res.sendStatus(204);
   }
 
   next();
